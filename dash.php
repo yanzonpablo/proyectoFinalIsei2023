7b813b1470682b8ea6654bd2858fd641031c19a0
yanzonpablo <yanzonpablo@gmail.com>
2023-06-28 09:26:23 -0300

    Correccion pantalla responsive caacitador

--- dash.php
+++ dash.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" href="images/logoCapacitacion.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/dash.css">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
